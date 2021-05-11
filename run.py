f56a518f8fc189eacddded1cd343a306e7628f19
Mikew1999 <mikewalters1234@gmail.com>
2021-05-11 23:19:26 +0000

    added basic navbar in base.html, added recipe and image content to index.html and extended from base.html

--- run.py
+++ run.py
@@ -10,6 +10,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/blog")
+def blog():
+    return render_template("blog.html", page_title="blog")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
